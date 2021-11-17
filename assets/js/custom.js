87399d69568dc7aba2070a2c7a983c944ee663ee
ali hosseini <31936745+aliho3eini@users.noreply.github.com>
2021-11-17 20:42:15 +0330

    base

---
+++ assets/js/custom.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+
+    const swiper = new Swiper('.swiper-header', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 1,
+        speed: 700,
+        autoplay: {
+        waitForTransition: true,
+        disableOnInteraction: false,
+        },
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+      });
+      
+
+    const swiperLogos = new Swiper('.swiper-logos', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 3,
+        speed: 1500,
+        autoplay: {
+        waitForTransition: true,
+        disableOnInteraction: false,
+        },
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+      });
+      
+
+
+      $(".mean-expand").on('click',function(){
+
+        console.log($(this).hasClass(' mean-clicked'));
+       
+        $(this).siblings('ul').toggleClass('d-flex');
+
+        // if($(this).hasClass(' mean-clicked') == false)
+        // {
+        //   $(this).siblings('ul').css('display','flex');
+        // }
+        // else{
+        //   $(this).siblings('ul').css('display','none');
+        // }
+        
+      })
+})
